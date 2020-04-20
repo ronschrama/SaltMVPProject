06ea90dab90288748dac11f782c4338b95ce4ef1
Ben Linnenbank <benlinnenbank92@gmail.com>
2020-04-20 15:51:17 +0200

    Feat: create login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
 const Router = require('koa-router');
+const validator = require('validator');
+const fetch = require('node-fetch');
 
 const router = new Router();
 
-
-router.get('/login', async (ctx) => {
+router.post('/login', async (ctx) => {
+  const { email, password } = ctx.request.body;
+  let data = await fetch('http://localhost:5000/DB.json');
+  data = await data.json();
+  const user = data.find((person) => person.email === email && person.password === password);
   ctx.status = 200;
   ctx.body = 'Hello!';
 });
--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const bodyParser = require('koa-bodyparser');
 const json = require('koa-json');
 const pg = require('pg');
 const users = require('./routes/users')
+const serve = require('koa-static');
 
 const app = new Koa();
 const port = 5000;
 
+app.use(serve('./public'));
 app.use(bodyParser());
 app.use(json());
 
 app.use(users.routes());
-
 
 
 if (!module.parent) {
