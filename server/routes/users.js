b0d606f63a88e08b31fa8660634cb0f40a98cabb
octavian-ilie <60600237+octavian-ilie@users.noreply.github.com>
2020-04-20 16:55:06 +0200

    feat: Update login route.

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,9 +8,31 @@ router.post('/login', async (ctx) => {
   const { email, password } = ctx.request.body;
   let data = await fetch('http://localhost:5000/DB.json');
   data = await data.json();
-  const user = data.find((person) => person.email === email && person.password === password);
-  ctx.status = 200;
-  ctx.body = 'Hello!';
+  const user = data.find((person) => person.email === email);
+  
+  if (typeof user === 'undefined') {
+    ctx.status = 403;
+    ctx.body = {
+      "status": "error",
+      "message": "Incorrect email address"
+    };
+  } else {
+    if (user.password !== password) {
+      ctx.status = 403;
+      ctx.body = {
+        "status": "error",
+        "message": "Incorrect password"
+      };
+    } else {
+      ctx.status = 200;
+      ctx.body = {
+        "status": "success",
+        "message": "Logged in"
+      };
+    }
+  }
 });
 
 module.exports = router;
+
+//  && person.password === password
