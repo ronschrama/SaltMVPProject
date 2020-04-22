43407ed0883155db9f50a4047c78a17bf40b2669
Ben Linnenbank <benlinnenbank92@gmail.com>
2020-04-22 10:35:15 +0200

    feat: created re-usable components

---
+++ client/src/components/CheckBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckBox(props) {
+  return (
+    <>
+      <input id="check" type="checkbox" />
+    </>
+  )
+}
+
+export default CheckBox
--- client/src/features/login/login.js
+++ client/src/features/login/login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loggingIn } from './loginSlice';
-import Button from '../../components/Button'
-
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import CheckBox from '../../components/CheckBox';
 
 function Login() {
   const dispatch = useDispatch();
@@ -12,6 +13,9 @@ function Login() {
   console.log(loginStatus);
   return (
     <div onClick={() => dispatch(loggingIn())}>
+      <Input />
+      <Input />
+      <CheckBox />
       <Button />
     </div>
   );
