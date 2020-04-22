cb11d5d1c4fb97fc99da343d07270ee33546f6b1
LiaPapailiou <LiaPapailiou@users.noreply.github.com>
2020-04-22 17:48:22 +0200

    fix: login component

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-const StyledButton = styled.button`
-  background-color: white;
-  font-size: 16px;
-  border-radius: 4px;
-  ${({ primary }) =>
-    primary && css`
-  width: 400px;
-  padding: 12px;
-  background-color: #3C78D8;
-  color: white;
-  `}
-   `;
+// const StyledButton = styled.button`
+//   background-color: white;
+//   font-size: 16px;
+//   border-radius: 4px;
+//   ${({ primary }) =>
+//     primary && css`
+//   width: 400px;
+//   padding: 12px;
+//   background-color: #3C78D8;
+//   color: white;
+//   `}
+//    `;
 
-function Button({ primary, children }) {
+function Button({ children }) {
   return (
-    <StyledButton primary={primary}>{children}</StyledButton>
+    <button>{children}</button>
   )
 }
 
--- client/src/features/login/login.js
+++ client/src/features/login/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loggingIn } from './loginSlice';
 import Button from '../../components/Button';
@@ -9,28 +9,33 @@ import Link from '../../components/Link';
 function Login() {
   const dispatch = useDispatch();
   const loginStatus = useSelector(
-    state => state.login.loggedIn
+    (state) => state.login.loggedIn,
   );
   console.log(loginStatus);
 
-  const [ input, setInput ] = useState([]);
+  const [userInput, setUserInput] = useState({ email: '', password: '' });
 
-  function getUserCredentials (e) {
-    const value = e.target.value;
-    setInput([...input, value]);
-    console.log(value);
+  function getUserCredentials(e) {
+    e.preventDefault();
+    // const refEl = useRef(null);
+    const { value } = e.target;
+    setUserInput({
+      ...userInput,
+      [e.target.user]: value,
+    });
   }
+  console.log(userInput);
 
   return (
     <div className="LoginForm">
       <h2>Welcome! Please login to your account</h2>
       <div onClick={() => dispatch(loggingIn())}>
-        <form onSubmit={() => getUserCredentials()}>
-          <Input value={input}>Email adress</Input>
-          <Input value={input}>Password</Input>
+        <form onSubmit={getUserCredentials}>
+          <Input value={userInput.email} user="email">Email adress</Input>
+          <Input value={userInput.password} user="password">Password</Input>
           <CheckBox>Remember me</CheckBox>
-          <Link>Forgot Password</Link>
-          <Button primary>Login</Button>
+          <Link href=" ">Forgot Password</Link>
+          <Button>Login</Button>
         </form>
       </div>
     </div>
