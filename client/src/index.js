21372ce59a293b159c0647aeafa134f76a78676b
Ron Schrama <ronschrama@Rons-MacBook-Pro.local>
2020-04-23 11:26:09 +0200

    feat: working on frontend login routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
+import getSession from './features/login/session';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Switch>
+          <Route exact path='/' render={() => (
+            getSession() ? (
+              <App to='/' />
+            ) : (
+                <Redirect to='/login' />
+              )
+          )} />
+        </Switch>
+      </Router>
+      {/* <App /> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
